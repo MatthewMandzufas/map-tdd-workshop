1185e4c04f3510abc95f5ac29be2005b3040fdf8
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-04-05 09:19:22 +0800

    GREEN: Map, maps over objects

--- src/map.js
+++ src/map.js
@@ -1,9 +1,25 @@
-function map(mapper, iterable) {
-    const mappedArray = [];
-    for (let i = 0; i < iterable.length; i++) {
-        mappedArray[i] = mapper(iterable[i]);
-    }
-    return mappedArray;
+function getIsPlainObject(valueToCheck) {
+    return (
+        valueToCheck !== null &&
+        typeof valueToCheck === 'object' &&
+        Object.getPrototypeOf(valueToCheck) === Object.prototype
+    );
 }
 
+function map(mapperFunction, functor) {
+    const isPlainObject = getIsPlainObject(functor);
+    if (Array.isArray(functor)) {
+        const mappedFunctor = [];
+        for (let i = 0; i < functor.length; i++) {
+            mappedFunctor[i] = mapperFunction(functor[i]);
+        }
+        return mappedFunctor;
+    } else if (isPlainObject) {
+        const mappedFunctor = {};
+        for (const [key, value] of Object.entries(functor)) {
+            mappedFunctor[key] = mapperFunction(value);
+        }
+        return mappedFunctor;
+    }
+}
 export default map;
